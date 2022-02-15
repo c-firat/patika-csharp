932a820ccf9d746260394835db36b987085d5116
cfirat <codecfirat@gmail.com>
2022-02-15 11:57:16 +0300

    if-else-if eklendi

---
+++ if-else-if/Program.cs
@@ -0,0 +1,12 @@
+﻿//If else if example
+int currentHour = DateTime.Now.Hour;
+if(currentHour > 8 && currentHour < 18){
+    Console.WriteLine("Have a nice work!");
+}
+else if(currentHour > 18 && currentHour < 23)
+{
+    Console.WriteLine("Good night");
+}
+else{
+    Console.WriteLine("Go to sleep");
+}
