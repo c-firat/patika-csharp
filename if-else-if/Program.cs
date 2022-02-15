64539564cf9a9a527986b3bdc550683b5c009804
cfirat <codecfirat@gmail.com>
2022-02-15 19:16:51 +0300

    ternary if eklendi

--- if-else-if/Program.cs
+++ if-else-if/Program.cs
@@ -1,12 +1,17 @@
 ﻿//If else if example
 int currentHour = DateTime.Now.Hour;
-if(currentHour > 8 && currentHour < 18){
+if (currentHour > 8 && currentHour < 18)
+{
     Console.WriteLine("Have a nice work!");
 }
-else if(currentHour > 18 && currentHour < 23)
+else if (currentHour > 18 && currentHour < 23)
 {
     Console.WriteLine("Good night");
 }
-else{
+else
+{
     Console.WriteLine("Go to sleep");
 }
+
+string result = currentHour >= 6 && currentHour <= 18 ? "Have a nice work" : "get some fun";
+Console.WriteLine(result);
