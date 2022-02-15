339f1434a992eaf93f30ad95efc8ef9798653748
cfirat <codecfirat@gmail.com>
2022-02-15 19:30:11 +0300

    Switch Case eklendi

---
+++ switch-case/Program.cs
@@ -0,0 +1,27 @@
+﻿int currMonth = DateTime.Now.Month;
+
+switch(currMonth){
+    case 12:
+    case 1:
+    case 2:
+        Console.WriteLine("Winter");
+        break;
+    case 3:
+    case 4:
+    case 5:
+        Console.WriteLine("Spring");
+        break;
+    case 6:
+    case 7:
+    case 8:
+        Console.WriteLine("Summer");
+        break;
+    case 9:
+    case 10:
+    case 11:
+        Console.WriteLine("Autumn");
+        break;
+    default:
+        Console.WriteLine("Error Case!");
+        break;
+}
