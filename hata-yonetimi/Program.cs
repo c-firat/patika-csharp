ee1c68b2be38662322dc6a92e0bbb0ed34cba0a7
cfirat <codecfirat@gmail.com>
2022-02-15 11:48:14 +0300

    Hata yonetimi eklendi

---
+++ hata-yonetimi/Program.cs
@@ -0,0 +1,19 @@
+﻿string num1;
+string num2;
+
+try{
+    Console.WriteLine("Num1: ");
+    num1 = Console.ReadLine();
+    Console.WriteLine("Num2: ");
+
+    num2 = Console.ReadLine();
+
+    int result = Convert.ToInt32(num1) + Convert.ToInt32(num2);
+    Console.WriteLine(result);
+}
+catch(Exception e){
+    Console.WriteLine(e.Message.ToString());
+}
+finally{
+    Console.WriteLine("Calculation is done!");
+}
