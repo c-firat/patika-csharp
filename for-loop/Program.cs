b0c1b40ff7ed1b1bb3d1f0dec2cf7f54252ffa20
cfirat <codecfirat@gmail.com>
2022-02-16 15:44:06 +0300

    For loop eklendi

---
+++ for-loop/Program.cs
@@ -0,0 +1,42 @@
+﻿//For loops Example
+Console.WriteLine("Type in zero to exit the code!");
+try{
+    for(;;){
+        Console.WriteLine("Number to check if it is prime:");
+        string numString = Console.ReadLine();
+        int num = Convert.ToInt32(numString);
+        if(num == 0)
+        {
+            Console.WriteLine("Program is terminated!");
+            return;
+        }
+        if(num < 2){
+            Console.WriteLine("It is not a prime number!");
+            continue;
+        }
+        else if (num == 2){
+            Console.WriteLine("It is a prime number!");
+            continue;
+        }
+
+        bool isPrime = true;
+        for(int i = 2; i < num; i++){
+            if(num % i == 0){
+                Console.WriteLine("It is not a prime number!");
+                isPrime = false;
+                break;
+            }
+        }
+
+        if(isPrime == true)
+            Console.WriteLine("It is a prime number!");
+    }
+}
+catch
+{
+    Console.WriteLine("Invalid value!");
+}
+
+
+
+
