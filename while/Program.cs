abb5c592626fb16c71a99a03d4c4f2041657446a
cfirat <codecfirat@gmail.com>
2022-02-20 20:54:22 +0300

    while and foreach examples were added

---
+++ while/Program.cs
@@ -0,0 +1,32 @@
+﻿//While and foreach loops
+Console.WriteLine("Type in a number to see cummulative sum till it.");
+try{
+    int num = int.Parse(Console.ReadLine());
+    int sum = 0;
+    int i = 0;
+    while(i < num){
+        sum += i;
+        i++;
+    }
+    Console.WriteLine("The sum is " + sum);
+}
+catch{
+    Console.WriteLine("Invalid input!");
+}
+
+Console.WriteLine("");
+
+Console.WriteLine("The letters a to z");
+char letter = 'a';
+while(letter <= 'z'){
+    Console.WriteLine(letter);
+    letter++;
+}
+
+Console.WriteLine("");
+
+string[] carModels = {"Honda", "Toyata", "Mercedes"};
+
+foreach(var models in carModels){
+    Console.WriteLine(models);
+}
